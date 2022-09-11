4c1b94077432c2199efc8fcadc19058c55e6a1df
Molta1 <ayubamolta@gmail.com>
2022-09-11 14:53:04 -0700

    y

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point, for code execution
+ * Return: Always 0 value (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point for code execution
+ * Return: Always 0 value(Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Sife of an int: %d byte(s)\n", sizeof(int));
+	printf("Sife of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Sife of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Sife of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
