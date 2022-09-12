6c0bc663bc07ac0456ce7eac3d90fafe04cc1429
Molta1 <ayubamolta@gmail.com>
2022-09-12 15:27:31 -0700

    g

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Point where execution begin
+ * Return: Always 0 value (Success)
+ */
+int main(0)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
