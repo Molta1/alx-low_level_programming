cf146141cbd698a238f8e539abfb236781627258
Molta1 <ayubamolta@gmail.com>
2022-09-12 16:06:37 -0700

    t

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point, where code execution begins
+ * Return: Always 0 value (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
